aa3e52580564a028a497b03b3598b1b3f3b989c6
Harrison Boateng <odameharrison13@gmail.com>
2023-02-03 23:44:37 +0000

    Done for today

---
+++ src/components/TypeWrite.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+
+function TypeWrite  ()  {
+    return (
+        <div className='text-center bg-black text-white md:p-80'>
+            <h1 className=''>
+                <Typewriter
+                options={{
+                    autoStart:true,
+                    loop:true,
+                    delay:50,
+                    pauseFor:1000,
+                    strings:["Hey There", "You are Welcome to Startup Lougue Africa page","To Learn More About Us", "Scroll Up"]
+                }}
+                />
+            </h1>
+        </div>
+    );
+}
+
+
+export default TypeWrite;
